1eaa3ec0c825fed731e1344dfff80d7d49ade168
justthatrandomcoder <me@juliusgrimm.dev>
2026-08-29 09:56:54 +0200

    initialize xcode project

---
+++ Steady/SteadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteadyApp.swift
+//  Steady
+//
+//  Created by  Julius Grimm on 29.08.26.
+//
+
+import SwiftUI
+
+@main
+struct SteadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
